d0e69689eb6c8f176ddf901aa236bb064e410506
TThe7Arts <160322399+TThe7Arts@users.noreply.github.com>
2024-02-23 10:32:01 +0100

    Update script.js
    Working

--- script.js
+++ script.js
@@ -2,3 +2,30 @@ function openMenu() {
     const nav = document.querySelector('nav');
     nav.classList.toggle('open');
 }
+emailjs.init("qug2o9aUAhMk5tXcy");
+
+const sendBtn = document.querySelector('.send-btn');
+const result = document.querySelector('.result');
+
+sendBtn.addEventListener('click', sendEmail);
+
+function sendEmail() {
+    // Get the form data
+    const to = document.getElementById("to").value;
+    const subject = document.getElementById("subject").value;
+    const message = document.getElementById("message").value;
+
+    // Send the email using EmailJS
+    emailjs.send("service_jgktcw3", "template_y8xr82k", {
+        to_email: to,
+        subject: subject,
+        message: message
+    })
+        .then(function () {
+            result.innerHTML = "Email sent successfully!";
+            result.style.opacity = 1;
+        }, function (error) {
+            result.innerHTML = "Email sending failed!";
+            result.style.opacity = 1;
+        });
+}
